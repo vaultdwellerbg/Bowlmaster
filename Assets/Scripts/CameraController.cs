4b6cc94122bc63fad53905c3aa7e4047b1982c59
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-10-25 19:00:18 +0300

    Follow ball with the camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public BallController ballController;
+
+	private Vector3 offset;
+
+	void Start () {
+		offset = transform.position - ballController.transform.position;
+	}
+	
+	void Update ()
+	{
+		if (BallBeforeFrontPin())
+		{
+			MoveCamera();
+		}
+	}
+
+	private bool BallBeforeFrontPin()
+	{
+		return ballController.transform.position.z < 1829f;
+	}
+
+	private void MoveCamera()
+	{
+		transform.position = ballController.transform.position + offset;
+	}
+}
