3f7efcddcff65dd23d9803764ea8a8b565cf6cbc
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-10-25 18:00:28 +0300

    Add rolling ball

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public float launchSpeed = 200f;
+
+	private AudioSource audioSource;
+
+	void Start ()
+	{
+		audioSource = GetComponent<AudioSource>();
+		Throw();
+	}
+
+	private void Throw()
+	{
+		GetComponent<Rigidbody>().velocity = new Vector3(0, -10f, launchSpeed);
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (!audioSource.isPlaying)
+		{
+			audioSource.Play();
+		}
+	}
+}
