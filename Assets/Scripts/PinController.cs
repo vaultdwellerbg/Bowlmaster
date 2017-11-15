b4a2ea24e6944acbdc6836484521d3f4cd12fad5
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-11-15 19:58:37 +0200

    Add detection for standing pin

---
+++ Assets/Scripts/PinController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinController : MonoBehaviour {
+
+	public float standingThreshold = 80f;
+
+	public bool IsStanding()
+	{
+		Vector3 eularAngles = transform.rotation.eulerAngles;
+		float xRotation = Mathf.Abs(eularAngles.x);
+		float zRotation = Mathf.Abs(eularAngles.z);
+
+		return xRotation < standingThreshold && zRotation < standingThreshold;
+	}
+}
