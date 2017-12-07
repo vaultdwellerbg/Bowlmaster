085214d3977d2dfa3b850ced6c25ae9d4ea91eb5
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-12-07 17:44:14 +0200

    Init score calculation with tests

---
+++ Assets/Scripts/Editor/ScoreManagerTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ScoreManagerTest {
+
+	private ScoreManager scoreManager;
+	private ScoreManager.Action endTurn;
+
+	[SetUp]
+	public void Init()
+	{
+		scoreManager = new ScoreManager();
+		endTurn = ScoreManager.Action.EndTurn;
+	}
+
+	[Test]
+	public void OneStrikeRetursnEndTurn() {
+		Assert.AreEqual(endTurn, scoreManager.Throw(10));
+	}
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+	public enum Action { Tidy, Reset, EndTurn, EndGame }
+
+	public Action Throw(int pins)
+	{
+		if (pins < 0 || pins > 10)
+		{
+			throw new UnityException("Invalid pin count for throw.");
+		}
+
+		Action action = Action.Tidy;
+		if (pins == 10)
+		{
+			action = Action.EndTurn;
+		}
+		return action;
+	}
+}
