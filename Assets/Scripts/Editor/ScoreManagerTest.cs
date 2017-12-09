caad4f7879c0a6aa8110474f6db0dcc5bef9c38c
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-12-09 17:01:55 +0200

    Return result for each scenario

--- Assets/Scripts/Editor/ScoreManagerTest.cs
+++ Assets/Scripts/Editor/ScoreManagerTest.cs
@@ -7,7 +7,7 @@ using System.Collections;
 public class ScoreManagerTest {
 
 	private ScoreManager scoreManager;
-	private ScoreManager.Action endTurn, tidy, reset;
+	private ScoreManager.Action endTurn, tidy, reset, endGame;
 
 	[SetUp]
 	public void Setup()
@@ -16,23 +16,118 @@ public class ScoreManagerTest {
 		endTurn = ScoreManager.Action.EndTurn;
 		tidy = ScoreManager.Action.Tidy;
 		reset = ScoreManager.Action.Reset;
+		endGame = ScoreManager.Action.EndGame;
 	}
 
 	[Test]
-	public void OneStrikeRetursnEndTurn() {
+	public void T01_OneStrikeRetursnEndTurn() {
 		Assert.AreEqual(endTurn, scoreManager.Throw(10));
 	}
 
 	[Test]
-	public void Throwing8ReturnsTidy()
+	public void T02_Throwing8ReturnsTidy()
 	{
 		Assert.AreEqual(tidy, scoreManager.Throw(8));
 	}
 
 	[Test]
-	public void ThrowingSpareReturnsEndTurn()
+	public void T03_ThrowingSpareReturnsEndTurn()
 	{
 		scoreManager.Throw(2);
+
 		Assert.AreEqual(endTurn, scoreManager.Throw(8));
 	}
+
+	[Test]
+	public void T04_ThrowingNoSpareOrStrikeOnLastFrameReturnsEndGame()
+	{
+		PlayNineFrames();
+		scoreManager.Throw(5);
+
+		Assert.AreEqual(endGame, scoreManager.Throw(1));
+	}
+
+	private void PlayNineFrames()
+	{
+		scoreManager.Throw(10);
+
+		scoreManager.Throw(9);
+		scoreManager.Throw(1);
+
+		scoreManager.Throw(5);
+		scoreManager.Throw(5);
+
+		scoreManager.Throw(7);
+		scoreManager.Throw(2);
+
+		scoreManager.Throw(10);
+
+		scoreManager.Throw(10);
+
+		scoreManager.Throw(10);
+
+		scoreManager.Throw(9);
+		scoreManager.Throw(0);
+
+		scoreManager.Throw(8);
+		scoreManager.Throw(2);
+	}
+
+	[Test]
+	public void T05_ThrowingStrikeOnLastFrameReturnsReset()
+	{
+		PlayNineFrames();
+
+		Assert.AreEqual(reset, scoreManager.Throw(10));
+	}
+
+	[Test]
+	public void T06_ThrowingSpareOnLastFrameReturnsReset()
+	{
+		PlayNineFrames();
+		scoreManager.Throw(3);
+
+		Assert.AreEqual(reset, scoreManager.Throw(7));
+	}
+
+	[Test]
+	public void T07_ThrowingOneBallAfterLastFrameSpareEndsGame()
+	{
+		PlayNineFrames();
+		scoreManager.Throw(3);
+		scoreManager.Throw(7);
+
+		Assert.AreEqual(endGame, scoreManager.Throw(6));
+	}
+
+	[Test]
+	public void T08_ThrowingStrikeBallAfterLastFrameSpareEndsGame()
+	{
+		PlayNineFrames();
+		scoreManager.Throw(3);
+		scoreManager.Throw(7);
+
+		Assert.AreEqual(endGame, scoreManager.Throw(10));
+	}
+
+	[Test]
+	public void T09_ThrowingTwoBallsAfterLastFrameStrikeEndsGame()
+	{
+		PlayNineFrames();
+		scoreManager.Throw(10);
+
+		scoreManager.Throw(6);
+		Assert.AreEqual(endGame, scoreManager.Throw(2));
+	}
+
+	[Test]
+	public void T10_ThrowingTwoStrikesAfterLastFrameStrikeEndsGame()
+	{
+		PlayNineFrames();
+		scoreManager.Throw(10);
+
+		scoreManager.Throw(10);
+		Assert.AreEqual(endGame, scoreManager.Throw(10));
+	}
+
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreManager : MonoBehaviour {
+public class ScoreManager {
 
 	public enum Action { Tidy, Reset, EndTurn, EndGame }
 
-	private int[] throws = new int[21];
+	private int[] throws = new int[22];
 	private int currentThrow = 1;
 
 	public Action Throw(int pins)
@@ -26,13 +26,50 @@ public class ScoreManager : MonoBehaviour {
 	{
 		if (IsStrike())
 		{
-			currentThrow += 2;
-			return Action.EndTurn;
+			if (IsLastFrame())
+			{
+				currentThrow += 2;
+				return Action.Reset;
+			}
+			else if (IsExtraThrow() && IsPrevThrowStrike())
+			{
+				currentThrow += 1;
+				return Action.Tidy;
+			}
+			else if (IsExtraThrow())
+			{
+				return Action.EndGame;
+			}
+			else
+			{
+				currentThrow += 2;
+				return Action.EndTurn;
+			}
 		}
 		else if (IsFirstBall())
 		{
+			if (IsExtraThrow() && IsPrevThrowSpare())
+			{
+				return Action.EndGame;
+			}
+			else
+			{
+				currentThrow += 1;
+				return Action.Tidy;
+			}
+		}
+		else if (IsLastFrame() && !IsSpare())
+		{
+			return Action.EndGame;
+		}
+		else if (IsLastFrame() && IsSpare())
+		{
 			currentThrow += 1;
-			return Action.Tidy;
+			return Action.Reset;
+		}
+		else if (IsExtraThrow())
+		{
+			return Action.EndGame;
 		}
 		else
 		{
@@ -50,4 +87,30 @@ public class ScoreManager : MonoBehaviour {
 	{
 		return currentThrow % 2 != 0;
 	}
+
+	private bool IsPrevThrowSpare()
+	{
+		return throws[currentThrow - 2] + throws[currentThrow - 3] == 10 && throws[currentThrow - 2] != 0;
+	}
+
+	private bool IsExtraThrow()
+	{
+		return currentThrow > 20;
+	}
+
+	private bool IsLastFrame()
+	{
+		return currentThrow > 18 && !IsExtraThrow();
+	}
+
+	private bool IsSpare()
+	{
+		return throws[currentThrow - 1] + throws[currentThrow - 2] == 10;
+	}
+
+	private bool IsPrevThrowStrike()
+	{
+		return throws[currentThrow - 2] + throws[currentThrow - 3] == 10 && throws[currentThrow - 2] == 0;
+	}
+
 }
