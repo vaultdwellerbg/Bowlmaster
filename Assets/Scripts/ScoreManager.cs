a110a656369f501edda8c9f5c1fd8a292ebdb946
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-12-30 22:22:35 +0200

    Add method to get score card like results

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 
 public class ScoreManager {
 
+	public static List<int> ScoreCumulative(List<int> rolls)
+	{
+		var scoreCardFields = new List<int>();
+		int currentScore = 0;
+		List<int> frameList = ScoreFrames(rolls);
+		foreach (int frameScore in frameList)
+		{
+			currentScore += frameScore;
+			scoreCardFields.Add(currentScore);
+		}
+		return scoreCardFields;
+	}
+
 	public static List<int> ScoreFrames(List<int> rolls)
 	{
 		var frameList = new List<int>();
