bae68599a5a0351d9eb7d7830ef429b9235bc3cf
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-12-30 22:22:58 +0200

    Add ScoreManager tests

---
+++ Assets/Scripts/Editor/ScoreManagerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using System.Linq;
+
+[TestFixture]
+public class ScoreManagerTest
+{
+	[Test]
+	public void T00PassingTest()
+	{
+		Assert.AreEqual(1, 1);
+	}
+
+}
