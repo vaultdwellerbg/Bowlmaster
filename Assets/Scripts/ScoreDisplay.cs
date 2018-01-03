a4fa73dd63bcb3cedb0777c218ec32aaf89595d7
Vaultdweller <vaultdwellerbg13@gmail.com>
2018-01-03 17:34:20 +0200

    Add score display script and wire it up to score texts

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+	public Text[] rollTexts, frameTexts;
+
+	private void Start()
+	{
+		for (int i = 0; i < rollTexts.Length - 1; i++)
+		{
+			rollTexts[i].text = "1";
+		}
+
+		for (int i = 0; i < frameTexts.Length; i++)
+		{
+			frameTexts[i].text = (i*2).ToString();
+		}
+	}
+}
