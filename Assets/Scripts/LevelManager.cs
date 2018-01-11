36324e26a63a44bf89c27a4c35802af8d843490c
Vaultdweller <vaultdwellerbg13@gmail.com>
2018-01-11 13:14:06 +0200

    Add basic menu system

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) 
+	{
+		Application.LoadLevel(name);
+	}
+	
+	public void LoadLevel(int index)
+	{
+		Application.LoadLevel(index);
+	}
+	
+	public void QuitRequest() 
+	{
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel()
+	{
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
---
+++ Assets/Scripts/StarsController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class StarsController : MonoBehaviour {
+
+	public enum OperationStatus {SUCCESS, FAILIURE};
+
+	private int totalStars = 60;
+	
+	void Start()
+	{
+		UpdateStarDisplay();
+	}
+
+	public void AddStars(int amount)
+	{
+		totalStars += amount;
+		UpdateStarDisplay();
+	}
+	
+	public OperationStatus SpendStars(int amount)
+	{		
+		if (totalStars >= amount)
+		{
+			totalStars -= amount;
+			UpdateStarDisplay();
+			return OperationStatus.SUCCESS;
+		}
+		return OperationStatus.FAILIURE;
+	}	
+	
+	private void UpdateStarDisplay()
+	{
+		GetComponent<Text>().text = totalStars.ToString();
+	}
+}
